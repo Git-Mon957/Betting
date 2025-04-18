1477cdc8fbf0402d5c5eb9020fb8aa353f234941
Git-mon957 <colonerik11@gmail.com>
2025-04-19 03:29:54 +0900

    Update Common.Infrastructure

--- src/Server/Common/Common.Infrastructure/Extensions/ConfigurationExtensions.cs
+++ src/Server/Common/Common.Infrastructure/Extensions/ConfigurationExtensions.cs
@@ -7,7 +7,6 @@ public static class ConfigurationExtensions
 {
     public static string GetDefaultConnectionString(
         this IConfiguration configuration)
-        => configuration.GetConnectionString("DefaultConnection");
 
     public static string GetCronJobsConnectionString(
         this IConfiguration configuration)
