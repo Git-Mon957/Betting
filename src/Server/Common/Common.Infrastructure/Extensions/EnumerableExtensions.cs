d8c24f0652b9ad4e3bb59ed9059e9768f70bd53e
Git-mon957 <colonerik11@gmail.com>
2025-04-19 03:30:19 +0900

    Update Common.Infrastructure

--- src/Server/Common/Common.Infrastructure/Extensions/EnumerableExtensions.cs
+++ src/Server/Common/Common.Infrastructure/Extensions/EnumerableExtensions.cs
@@ -7,7 +7,6 @@ public static class EnumerableExtensions
 {
     public static void ForEach<T>(
         this IEnumerable<T> enumerable,
-        Action<T> action)
     {
         foreach (var item in enumerable)
         {
